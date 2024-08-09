48c4165da118d7979e079d31ebe45e109d38c327
Nourhen <nourhen452@gmail.com>
2024-08-09 17:30:00 +0100

    done

--- frontend/app/navBar/page.tsx
+++ frontend/app/navBar/page.tsx
@@ -9,6 +9,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import Badge from '@mui/material/Badge';
 import axios from "axios";
+
 import "./nav.css"
 
 interface Product {
@@ -72,15 +73,14 @@ const Navbar: React.FC =()=>{
       };
 
   return (
-    <div>
+   
+    <div >
 
 
-    <div className='fixed top-0 w-full  z-10'>
-       <div className='w-full h-[40px] flex justify-center items-center bg-black'>
-       </div>
+    <div className=' fixed top-0 w-full  z-10  '>
        <div>
-        <div className='flex justify-between items-center p-6 text-[16px] md:text-[20px]'>
-<div className='hidden md:flex flex-1 space-x-7'>
+        <div className=' navi flex justify-between items-center p-6 text-[16px] md:text-[20px]'>
+<div className=' hidden md:flex flex-1 space-x-7'>
     <a href="/home" onClick={() => handleItemClick(1)}>Home</a>
     <a href="/contact" onClick={() => handleItemClick(1)}>Contact</a>
 <a href="/about" onClick={() => handleItemClick(1)}>Why Us</a>
@@ -102,7 +102,7 @@ const Navbar: React.FC =()=>{
   <div className="mt-1 gap-[3px] flex justify-center align-center">
 
  <button onClick={toggleSearch}>
-  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" className="cursor-pointer mt-0.5" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M443.5 420.2L336.7 312.4c20.9-26.2 33.5-59.4 33.5-95.5 
+  <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" className="cursor-pointer mt-0.5" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M443.5 420.2L336.7 312.4c20.9-26.2 33.5-59.4 33.5-95.5 
 0-84.5-68.5-153-153.1-153S64 132.5 64 217s68.5 153 153.1 153c36.6 0 70.1-12.8 96.5-34.2l106.1 107.1c3.2 3.4 7.6 5.1 11.9 5.1 4.1 0 8.2-1.5 11.3-4.5 6.6-6.3 6.8-16.7.6-23.3zm-226.4-83.1c-32.1 0-62.3-12.5-85-35.2-22.7-22.7-35.2-52.9-35.2-84.9 0-32.1 12.5-62.3 35.2-84.9 22.7-22.7 52.9-35.2 85-35.2s62.3 12.5 85 35.2c22.7 22.7 35.2 52.9 35.2 84.9 0 32.1-12.5 62.3-35.2 84.9-22.7 22.7-52.9 35.2-85 35.2z"></path>
 </svg></button>
 </div>
@@ -120,8 +120,10 @@ const Navbar: React.FC =()=>{
         </div>
        </div>
     </div>
+   
     </div>
-
+    
+    
 
   )
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
 import Head from "./navBar/page"
+import Bodyhome from "./bodyhome/page";
 export default function Home() {
   return (
    <div className="body">
 
 <Head/>
+
+<Bodyhome/>
+
    </div>
   );
 }
